b2b02deaa49895a5e1fe1c10516002cbf7abc852
paaulaagarciiaa <p.garciar.2019@alumnos.urjc.es>
2023-03-06 12:49:02 +0100

    empiezo js p1

---
+++ P1/tienda.js
@@ -0,0 +1,70 @@
+//-- Definición de modulos y puerto
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+
+const PUERTO = 9000;
+
+
+//-- Creo el servidor
+const server = http.createServer((req, res) => {
+
+  console.log('\nPetición recibida');
+
+  //-- Construir el objeto url con la url de la solicitud
+  let myURL = new URL(req.url, 'http://' + req.headers['host']);
+  console.log(" * URL completa: " + myURL.href);
+    // * Pathname devuelve el nombre de ruta de una URL
+  console.log(" * Ruta: " + myURL.pathname);
+
+  //-- Fichero del recurso
+  let file = "";
+
+  //-- Analizar el recurso solicitado
+  if (myURL.pathname == '/') {
+    file = '/tienda.html';
+  }else{
+    file = myURL.pathname;
+  }
+
+  type_file = file.split(".")[1];
+  file = '.' + file;
+  console.log(' * Recurso solicitado: ' + file);
+  console.log(' * Extensión del recurso solicitado: ' + type_file);
+
+  //-- Tipo de archivo
+  const mime_type = {
+    'html' : 'text/html',
+    'css'  : 'text/css',
+    'jpg'  : 'image/jpg',
+    'ico'  : 'image/x-icon',
+    'png'  : 'image/png',
+  };
+
+  let mime = mime_type[type_file];
+  console.log(' * Tipo de MIME: ' + mime);
+
+  //-- Lectura asincrona del fichero
+  fs.readFile(file, (err,data) => {
+    //-- Fichero no encontrado, página de error
+    if (err) {
+      // * La página de error es un MIME tipo 'html'
+      res.writeHead(404, {'Content-Type': 'text/html'});
+      console.log('\n404: Not Found');
+      file = './error.html';
+      data = fs.readFileSync(file);
+      res.write(data);
+      res.end();
+    }else{
+      res.writeHead(200, {'Content-Type': mime});
+      console.log('\n200: OK');
+      res.write(data);
+      res.end();
+    }
+  });
+});
+
+//-- Activo el servidor
+server.listen(PUERTO);
+
+console.log("Escuchando en puerto: " + PUERTO + '\n');
