fcf4f237ed83386dca3aa4b9aebe84caaa2b1697
paaulaagarciiaa <p.garciar.2019@alumnos.urjc.es>
2023-05-08 12:02:49 +0200

    p3

--- P3/public/chat-client.js
+++ P3/public/chat-client.js
@@ -1,20 +1,35 @@
 //-- Elementos del interfaz
+const button = document.getElementById("button");
 const display = document.getElementById("display");
-const msg_entry = document.getElementById("msg_entry");
 
 //-- Crear un websocket. Se establece la conexión con el servidor
 const socket = io();
 
+let contador = 1;
+
+socket.on("connect", () => {
+  //-- Enviar mensaje inicial
+  socket.send("Mensaje inicial del Cliente!!!");
+});  
+
+socket.on("disconnect", ()=> {
+  display.innerHTML="¡¡DESCONECTADO!!"
+})
 
 socket.on("message", (msg)=>{
   display.innerHTML += '<p style="color:blue">' + msg + '</p>';
 });
 
 //-- Al apretar el botón se envía un mensaje al servidor
-msg_entry.onchange = () => {
-  if (msg_entry.value)
-    socket.send(msg_entry.value);
-  
-  //-- Borrar el mensaje actual
-  msg_entry.value = "";
+button.onclick = () => {
+  socket.send("Holiii-" + contador);
+  contador += 1;
 }
+
+let tic = 1;
+
+//-- Enviar un mensaje periódico ("TIC")
+setInterval(() => {
+    socket.emit('tic', "TIC-" + tic);
+    tic += 1;
+}, 1000);
