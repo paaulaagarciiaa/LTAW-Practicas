5f99b6b8872ec974ab4195d8374689a475d350f1
paaulaagarciiaa <p.garciar.2019@alumnos.urjc.es>
2023-05-22 14:34:09 +0200

    p4

--- P3/client2.js
+++ P3/client2.js
@@ -1,32 +1,20 @@
 //-- Elementos del interfaz
-const display = document.getElementById('display');
-const msg_entry = document.getElementById('msg_entry');
+const display = document.getElementById("display");
+const msg_entry = document.getElementById("msg_entry");
 
 //-- Crear un websocket. Se establece la conexión con el servidor
 const socket = io();
 
-//-- Escuchar cuando un nuevo mensaje es recibido
-socket.on('nuevo mensaje', (mensaje) => {
-  display.innerHTML += '<p><strong>' + mensaje.remitente + ': </strong>' + mensaje.mensaje + '</p>';
-});
 
-//-- Escuchar cuando un nuevo usuario se conecta al chat
-socket.on('usuario conectado', (usuario) => {
-  display.innerHTML += '<p style="color:green">' + usuario + ' se ha conectado al chat</p>';
-});
-
-//-- Escuchar cuando un usuario se desconecta del chat
-socket.on('usuario desconectado', (usuario) => {
-  display.innerHTML += '<p style="color:red">' + usuario + ' se ha desconectado del chat</p>';
+socket.on("message", (msg)=>{
+  display.innerHTML += '<p style="color:blue">' + msg + '</p>';
 });
 
 //-- Al apretar el botón se envía un mensaje al servidor
 msg_entry.onchange = () => {
-  if (msg_entry.value) {
-    //-- Enviar el mensaje al servidor
-    socket.emit('nuevo mensaje', msg_entry.value);
-    
-    //-- Borrar el mensaje actual
-    msg_entry.value = '';
-  }
-};
+  if (msg_entry.value)
+    socket.send(msg_entry.value);
+  
+  //-- Borrar el mensaje actual
+  msg_entry.value = "";
+}
--- P3/server2.js
+++ P3/server2.js
@@ -1,15 +1,25 @@
+//-- Cargar las dependencias
+const socket = require('socket.io');
+const http = require('http');
 const express = require('express');
+const colors = require('colors');
+
+const PUERTO = 8090;
+
+//-- Crear una nueva aplciacion web
 const app = express();
-const server = require('http').createServer(app);
-const io = require('socket.io')(server);
 
-//-- Mantener un registro de los usuarios conectados
-const usuarios_conectados = new Set();
+//-- Crear un servidor, asosiaco a la App de express
+const server = http.Server(app);
 
+//-- Crear el servidor de websockets, asociado al servidor http
+const io = socket(server);
+
+//-------- PUNTOS DE ENTRADA DE LA APLICACION WEB
 //-- Definir el punto de entrada principal de mi aplicación web
 app.get('/', (req, res) => {
-    res.send('Bienvenido a mi aplicación Web!!!' + '<p><a href="/chat.html">Test</a></p>');
-  });
+  res.send('Bienvenido a mi aplicación Web!!!' + '<p><a href="/chat2.html">Comencemos</a></p>');
+});
 
 //-- Esto es necesario para que el servidor le envíe al cliente la
 //-- biblioteca socket.io para el cliente
@@ -18,37 +28,28 @@ app.use('/', express.static(__dirname +'/'));
 //-- El directorio publico contiene ficheros estáticos
 app.use(express.static('public'));
 
+//------------------- GESTION SOCKETS IO
+//-- Evento: Nueva conexion recibida
+io.on('connect', (socket) => {
+  
+  console.log('** NUEVA CONEXIÓN **'.yellow);
 
-io.on('connection', (socket) => {
-  console.log('Un usuario se ha conectado');
+  //-- Evento de desconexión
+  socket.on('disconnect', function(){
+    console.log('** CONEXIÓN TERMINADA **'.yellow);
+  });  
 
-  //-- Añadir el usuario a la lista de usuarios conectados
-  usuarios_conectados.add(socket.id);
+  //-- Mensaje recibido: Reenviarlo a todos los clientes conectados
+  socket.on("message", (msg)=> {
+    console.log("Mensaje Recibido!: " + msg.blue);
 
-  //-- Notificar a todos los usuarios conectados que un nuevo usuario se ha unido
-  io.emit('usuario conectado', socket.id);
-
-  socket.on('disconnect', () => {
-    console.log('Usuario desconectado');
-
-    //-- Eliminar al usuario de la lista de usuarios conectados
-    usuarios_conectados.delete(socket.id);
-
-    //-- Notificar a todos los usuarios conectados que un usuario se ha desconectado
-    io.emit('usuario desconectado', socket.id);
+    //-- Reenviarlo a todos los clientes conectados
+    io.send(msg);
   });
 
-  socket.on('nuevo mensaje', (mensaje) => {
-    console.log('Nuevo mensaje recibido: ' + mensaje);
-
-    //-- Enviar el mensaje a todos los usuarios conectados (excepto al remitente)
-    socket.broadcast.emit('nuevo mensaje', {
-      remitente: socket.id,
-      mensaje: mensaje
-    });
-  });
 });
 
-server.listen(3000, () => {
-  console.log('Servidor iniciado en el puerto 3000');
-});
+//-- Lanzar el servidor HTTP
+//-- ¡Que empiecen los juegos de los WebSockets!
+server.listen(PUERTO);
+console.log("Escuchando en puerto: " + PUERTO);
