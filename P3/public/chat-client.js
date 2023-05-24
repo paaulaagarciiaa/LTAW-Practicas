06a3ddba445206f6e4f58fdf59ebe0939afa9cd7
paaulaagarciiaa <p.garciar.2019@alumnos.urjc.es>
2023-05-24 23:22:47 +0200

    Falta comandos especiales P3

--- P3/public/chat-client.js
+++ P3/public/chat-client.js
@@ -7,10 +7,18 @@ const username = prompt("Ingrese su nombre de usuario:");
 //-- Crear un websocket. Se establece la conexión con el servidor
 const socket = io();
 
+// Variable de tono que suene cuando se envíe un mensaje
+let tono = new Audio('tono.mp3');
+
 socket.on("connect", () => {
   //-- Enviar mensaje inicial
   socket.send(`${username} se unió al chat!`);
 });
+
+socket.on("disconnect", () => {
+  //-- Enviar mensaje inicial
+  socket.send(`Usuario desconectado`);
+});  
 
 // Mostrar un nuevo mensaje en la pantalla
 const agregarMensaje = (mensaje) => {
@@ -27,18 +35,25 @@ socket.on("message", (msg)=>{
 // Mostrar el nombre del usuario en la pantalla
 const nombreUsuario = document.createElement("div");
 nombreUsuario.textContent = `Bienvenido al chat, ${username}!`;
+nombreUsuario.style.fontFamily = "Arial";
+nombreUsuario.style.fontSize = "15px";
+nombreUsuario.style.color = "white";
+nombreUsuario.style.padding = "15px";
+nombreUsuario.style.backgroundColor = "black";
+nombreUsuario.style.marginLeft = "35%";
+nombreUsuario.style.marginRight = "35%";
 document.body.appendChild(nombreUsuario);
 
 //-- Al apretar el botón se envía un mensaje al servidor
 msg_entry.onchange = () => {
   if (msg_entry.value)
-    socket.send(msg_entry.value);
-  
+  socket.send(msg_user.value + ':' + '' + msg_entry.value);
   //-- Borrar el mensaje actual
   msg_entry.value = "";
+  tono.play();
 }
 
-// Al cambiar el valor usuario
+// --Al cambiar el valor usuario
 usuario.onchange = () => {
   if (usuario.value )
   // el usuario pasa a llamarse como se ha declarado
